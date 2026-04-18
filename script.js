b45739336595b50472f79a49eec5a8df8013a323
Sarah Luong <sarahluong123@hotmail.com>
2026-04-17 23:55:48 -0700

    added canucks pointer

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// cursor
+const cursor = document.querySelector(".fake-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
 /* ─────────────────────────────────────────────
    NAV DOTS
 ───────────────────────────────────────────── */
